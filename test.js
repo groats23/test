8f026e9dde9a64d1598a0f7cd9d569cc56db93ea
groats23 <121118901+groats23@users.noreply.github.com>
2023-03-10 10:26:58 +0100

    update

---
+++ test.js
@@ -0,0 +1,9 @@
+const age = 10;
+const totalPrice = 220.74;
+const userName = "Chelsy";
+const message = "Willcomen"
+const isOpen = true;
+const shouldConfirm = false;
+
+console.log("Цена", totalPrice);
+
