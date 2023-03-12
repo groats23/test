79513185708755a3c60c3cf6ca6efebea149d0d1
groats23 <121118901+groats23@users.noreply.github.com>
2023-03-12 09:19:46 +0100

    1

--- test.js
+++ test.js
@@ -1,9 +1,32 @@
-const age = 10;
-const totalPrice = 220.74;
-const userName = "Chelsy";
-const message = "Willcomen"
-const isOpen = true;
-const shouldConfirm = false;
+// const age = 10;
+// const totalPrice = 220.74;
+// const userName = "Chelsy";
+// const message = "Willcomen"
+// const isOpen = true;
+// const shouldConfirm = false;
 
-console.log("Цена", totalPrice);
+// console.log("Цена", totalPrice);
+
+
+// const shouldRenew = confirm("Хотите продлить подписку?");
+// console.log(shouldRenew);
+
+// let quantity = prompt("Введите количество товаров")
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = "50px";
+// elementWidth = Number.parseInt(elementWidth);
+// console.log("elementWidth:", elementWidth);
+
+// let elementWidth = "50.24px";
+// elementWidth = Number.parseFloat(elementWidth);
+// console.log("elementWidth:", elementWidth);
+
+
+// let salary = 1300.13213213;
+// salary = Number(salary.toFixed(3));
+
+// console.log(salary);
 
